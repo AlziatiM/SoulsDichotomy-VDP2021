4ce997b735e67d273d7cd279665128fe6e423ddc
Michele Alziati <61688148+AlziatiM@users.noreply.github.com>
2021-11-12 13:30:17 +0100

    Implemented camera following

--- SoulsDichotomy/Assets/Scripts/LevelStarter.cs
+++ SoulsDichotomy/Assets/Scripts/LevelStarter.cs
@@ -12,14 +12,16 @@ public class LevelStarter : MonoBehaviour
     public GameObject player;
     public GameObject soul;
     public GameObject startPoint;
-
+    public GameObject cameraFollow;
+    private CinemachineVirtualCamera camera_vc;
+    
     private void Awake()
     {
         Instantiate(gameManager, Vector3.zero, Quaternion.identity);
         Instantiate(skillManager, Vector3.zero, Quaternion.identity);
         GameObject go1 = Instantiate(player, startPoint.transform.position, Quaternion.identity);
         GameObject go2 = Instantiate(soul, startPoint.transform.position, Quaternion.identity);
-        FindObjectOfType<Camera>().gameObject.transform.SetParent(go1.transform);
+        camera_vc = Instantiate(cameraFollow, Vector3.zero, Quaternion.identity).GetComponent<CinemachineVirtualCamera>();
         SkillManager.instance.SetUpCharacters(go1.GetComponent<PlayerInput>(), go2.GetComponent<SoulController>());
     }
 
