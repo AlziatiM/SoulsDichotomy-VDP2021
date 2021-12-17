afdd931252b96ff830c412dd47d33025a21afec5
Matteo Cordioli <79856894+MatteoCordioli@users.noreply.github.com>
2021-12-17 11:47:46 +0100

    Add Button level selector code to make it more clear

---
+++ SoulsDichotomy/Assets/Scripts/Menu/LevelSelector/LevelButtonSelector.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class LevelButtonSelector : MonoBehaviour
+{
+    [Header("Level the Door will open")]
+    [SerializeField] private int level;
+    [Header("Sprites")]
+    [SerializeField] private Sprite locked;
+    [SerializeField] private Sprite unlocked;
+    [Header("UI components")]
+    [SerializeField] private TextMeshProUGUI textMeshPro;
+    [SerializeField] private Image imageUI;
+    private Button button;
+    private void Awake()
+    {
+        button = gameObject.GetComponent<Button>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUp();
+    }
+
+    private void OnEnable()
+    {
+        SetUp();
+    }
+
+    private void SetUp()
+    {
+        //Level1 has 1 if is already be completed 0 otherwise, so you can re-choose it
+        //Level0 must have 1 from the beginning
+        if (PlayerPrefs.HasKey("Level" + level))
+        {
+            if (PlayerPrefs.GetInt("Level" + level) == 1)
+            {
+                
+
+                imageUI.sprite = unlocked;
+                button.interactable = true;
+            }
+            else
+            {
+                imageUI.sprite = locked;
+                button.interactable = false;
+            }
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Level" + level, level==0?1:0);
+            SetUp();
+        }
+        textMeshPro.text = level.ToString();
+    }
+}
