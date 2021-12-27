d01a49f2e95af02d3f140a2e5594b55bdfd23573
Matteo Cordioli <79856894+MatteoCordioli@users.noreply.github.com>
2021-12-27 20:10:15 +0100

    Add delay to activate crossbow and art to it

--- SoulsDichotomy/Assets/Scripts/ObjectReactor/Crossbow.cs
+++ SoulsDichotomy/Assets/Scripts/ObjectReactor/Crossbow.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class Crossbow : MonoBehaviour, IReact
 {
+    [Header("From close to open")]
+    public Sprite[] sprites;
+
     [SerializeField] private float timeBetweenBullets;
+    [SerializeField] private float timeBeforReact;
     [SerializeField] private bool amIActive;
     [SerializeField] private bool whenActiveAlwaysShot;
     [SerializeField] private int numOfBullet;
     public GameObject bullet;
     public GameObject firePoint;
 
+    private float toWaitAnim;
+    private SpriteRenderer spriteRenderer;
     private void OnValidate()
     {
         if (whenActiveAlwaysShot)
@@ -21,9 +27,12 @@ public class Crossbow : MonoBehaviour, IReact
 
     private void Start()
     {
+        toWaitAnim = timeBetweenBullets / (2 * (sprites.Length - 1));
+        spriteRenderer= this.gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprites[0];
         if (amIActive)
         {
-            StartCoroutine("StartShot");
+            StartCoroutine("StartToReact");
         }
     }
 
@@ -32,12 +41,18 @@ public class Crossbow : MonoBehaviour, IReact
         amIActive = !amIActive;
         if (amIActive)
         {
-            StartCoroutine("StartShot");
+            StartCoroutine("StartToReact");
         }
         else
         {
             StopAllCoroutines();
         }
+    }
+
+    private IEnumerator StartToReact()
+    {
+        yield return new WaitForSeconds(timeBeforReact);
+        StartCoroutine("StartShot");
     }
 
     private IEnumerator StartShot()
@@ -46,7 +61,7 @@ public class Crossbow : MonoBehaviour, IReact
         {
             while (true)
             {
-                Instantiate(bullet, firePoint.transform.position, firePoint.transform.rotation);
+                StartCoroutine("OpenCross");
                 yield return new WaitForSeconds(timeBetweenBullets);
             }
         }
@@ -55,13 +70,34 @@ public class Crossbow : MonoBehaviour, IReact
             int cont = 0;
             while (cont < numOfBullet)
             {
-                Instantiate(bullet, firePoint.transform.position, firePoint.transform.rotation);
+                StartCoroutine("OpenCross"); 
                 yield return new WaitForSeconds(timeBetweenBullets);
                 cont++;
             }
             amIActive = false;
         }
         
+    }
+
+    private IEnumerator OpenCross()
+    {
+        
+        for(int i=1; i<sprites.Length; i++)
+        {
+            spriteRenderer.sprite = sprites[i];
+            yield return new WaitForSeconds(toWaitAnim);
+        }
+        Instantiate(bullet, firePoint.transform.position, firePoint.transform.rotation);
+        StartCoroutine("CloseCross");
+    }
+
+    private IEnumerator CloseCross()
+    {
+        for (int i = sprites.Length-1; i >= 0; i--)
+        {
+            spriteRenderer.sprite = sprites[i];
+            yield return new WaitForSeconds(toWaitAnim);
+        }
     }
 
 }
